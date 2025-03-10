f57b2d712325a359e448aa4e189e3e7e678ac601
Autumn Hoerr <autumn.hoerr@powerschool.com>
2025-03-10 12:37:53 -0400

    all_descendants working

--- lib/pagebuilder/menu_items.ex
+++ lib/pagebuilder/menu_items.ex
@@ -3,15 +3,24 @@ defmodule Pagebuilder.MenuItems do
   lkajhsdflkajsflk
 
   """
-
+  import Ecto.Query
   alias Pagebuilder.MenuItem
   alias Pagebuilder.Repo
 
-  def list_menu_items() do
+  def list_menu_items do
     Repo.all(MenuItem)
   end
 
   def change_menu_item(menu_item \\ %MenuItem{}, attrs \\ %{}) do
     MenuItem.changeset(menu_item, attrs)
+  end
+
+  def all_descendants_of(menu_item_id) do
+    to_look = "*.#{menu_item_id}.*"
+
+    from(mi in MenuItem,
+      where: fragment("?::ltree ~ ?::lquery", mi.path, ^to_look)
+    )
+    |> Repo.all()
   end
 end
